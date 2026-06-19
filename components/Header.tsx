8d6a9b8dd65d5000a98fe2c280ed831cdd897485
negiarnav03 <negijiarnav@gmail.com>
2026-06-19 16:33:34 +0530

    done with the hero page.

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,8 +2,10 @@ import Link from 'next/link'
 import React from 'react'
 import Image from 'next/image'
 import { ArrowRight, Ghost, Zap } from 'lucide-react';
-import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs';
+import {Show, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs';
 import { Button } from './ui/button';
+import PricingModal from './ui/PricingModal';
+
 
 const Header = () => {
   return (
@@ -23,10 +25,12 @@ const Header = () => {
             > 
             Projects</Link>
 
+<PricingModal>
             <span className='inline-flex h-8 items-center gap-1.5 rounded-full border border-white/10 bg-white/5 px-3 text-xs text-white/70'>
                 <Zap className='h-3 w-3 fill-white/70'/>
                 3 / 40 credits
             </span>
+</PricingModal>
             <UserButton />
             </Show>
 
---
+++ components/ui/PricingModal.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import React from 'react'
+import {
+    Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger
+} from "@/components/ui/dialog"
+import { BlueTitle } from '../resuables';
+import { PricingTable } from '@clerk/nextjs';
+
+interface PricingModalProps {
+    children: React.ReactNode;
+    reason?: "credits" | "upgrade";
+}
+
+const PricingModal = ({ children, reason = "upgrade" }: PricingModalProps) => {
+
+    const title =
+        reason === "credits" ? "You're out of credits" : "Upgrade your plan";
+
+    const description =
+        reason === "credits"
+            ? "Buy more credits to continue building"
+            : "Upgrade to access premium features and higher limits.";
+
+
+
+    return (
+        <Dialog>
+            <DialogTrigger className={"cursor-pointer"}>{children}</DialogTrigger>
+            <DialogContent 
+            className={"border-white/8 bg-[#0f0f0f] p-0 text-white sm:max-w-6xl max-h-[90dvh] overflow-y-auto"
+
+            }>
+                <DialogHeader className='px-6 pt-6 pb-2'>
+                    <DialogTitle className="font-serif text-xl tracking-tight.text-white/90 ">Are you absolutely sure?</DialogTitle>
+                    <BlueTitle className='text-4xl'>{title}</BlueTitle>
+                    <DialogDescription className='text-sm text-white/35'>
+                        {description}
+                    </DialogDescription>
+                </DialogHeader>
+
+            <div className='px-6 pb-6'>
+                <PricingTable
+            checkoutProps={{
+              appearance: {
+                elements: {
+                  drawerRoot: {
+                    zIndex: 2000,
+                  }
+                }
+              }
+            }} />
+            </div>
+
+            </DialogContent>
+        </Dialog>
+    )
+}
+
+export default PricingModal;
